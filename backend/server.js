bd4077903f0c2ad936140cc6c9835abfb7341efe
praveersingh <spraveer135@gmail.com>
2026-04-23 04:07:48 +0530

    fix render port issue

--- backend/server.js
+++ backend/server.js
@@ -29,4 +29,6 @@ app.get("/", (req, res) => {
 app.use("/api", authRoutes);
 
 // START SERVER
-app.listen(5000, () => console.log("Server running on port 5000"));
+const PORT = process.env.PORT || 5000;
+
+app.listen(PORT, () => console.log(`Server running on port ${PORT}`));
